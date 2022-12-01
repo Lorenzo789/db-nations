c4997b9ad79cebc8fe120ae499c4dff0563d001d
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-12-01 10:56:26 +0100

    milestone 2 and bonus complete but to refine

--- src/main/java/org/generation/italy/Main.java
+++ src/main/java/org/generation/italy/Main.java
@@ -16,7 +16,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 //		milestone1();
-		milestone2();
+//		milestone2();
+		bonus();
 	}
 	
 	public static void milestone1() {
@@ -77,7 +78,11 @@ public class Main {
 					
 					while(rs.next()) {
 				
-						System.out.println();
+						final int countryId = rs.getInt(1);
+						final String countryName = rs.getString(2);
+						
+						System.out.println(countryId + " - "
+								+ countryName);
 					}
 				}
 			} 
@@ -88,13 +93,90 @@ public class Main {
 		
 			}
 		}
-	}
 	
-//	SELECT *
-//	FROM countries
-//	WHERE countries.name
-//	LIKE "%ita%";
-
+	public static void bonus() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Insert a nation name");
+		String inputCountry = sc.nextLine();
+		
+		try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD)) {
+			
+			final String sql = "SELECT * FROM countries WHERE countries.name LIKE ?";
+	
+			try (PreparedStatement ps = con.prepareStatement(sql)) {
+				
+				ps.setString(1, "%" + inputCountry + "%");
+				
+				try (ResultSet rs = ps.executeQuery()) {
+					
+					while(rs.next()) {
+				
+						final int countryId = rs.getInt(1);
+						final String countryName = rs.getString(2);
+						
+						System.out.println(countryId + " - "
+								+ countryName);
+					}
+				}
+			} 
+	
+			} catch (Exception e) {
+	
+			System.err.println("ERROR: " + e.getMessage());
+		
+			}
+		
+		System.out.println("Insert an id");
+		int inputId = sc.nextInt();
+		
+		sc.close();
+		
+		try (Connection con = DriverManager.getConnection(URL, USER, PASSWORD)) {
+			
+			final String sqlLang = " SELECT countries.country_id, countries.name, country_stats.population, country_stats.gdp, languages.`language` "
+					+ " FROM countries "
+					+ " JOIN country_languages "
+					+ " ON country_languages.country_id = countries.country_id "
+					+ " JOIN languages "
+					+ " ON country_languages.language_id = languages.language_id "
+					+ " JOIN country_stats "
+					+ " ON country_stats.country_id = countries.country_id "
+					+ " WHERE countries.country_id = ? "
+					+ " GROUP BY countries.country_id, countries.name, country_stats.population, country_stats.gdp, languages.`language` ";
+	
+			try (PreparedStatement ps = con.prepareStatement(sqlLang)) {
+				
+				ps.setInt(1, inputId);
+				
+				try (ResultSet rs = ps.executeQuery()) {
+					
+					while(rs.next()) {
+				
+						final int countryId = rs.getInt(1);
+						final String countryName = rs.getString(2);
+						final long countryPopulation = rs.getLong(3);
+						final long countryStats = rs.getLong(4);
+						final String countryLanguage = rs.getString(5);
+						
+						System.out.println(countryId + " - "
+								+ countryName + " - "
+								+ countryPopulation + " - "
+								+ countryStats + " - "
+								+ countryLanguage);
+					}
+				}
+			} 
+	
+			} catch (Exception e) {
+	
+			System.err.println("ERROR: " + e.getMessage());
+		
+			}
+		
+		}	
+}
 
 /*
 try (Connection con = DriverManager.getConnection(URL, USER, PWS)) {
